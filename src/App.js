daa28ce79f94c2b99fb9d80c60887d10cdbc20b3
aksrud <aksrud06@gmail.com>
2025-03-27 04:53:56 +0900

    [init] 학복스스

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
+
+  const onClickEvent = () => {
+    window.location.replace("https://www.naver.com/");
+  }
+  const imageUrl = "https://static.univstore.com/web/image/logo.svg"
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <HomeLogo onClick={()=>{
+        onClickEvent()}
+      }>
+        <img src={imageUrl} style={{width:"281px",height:'88px'}}></img>
+      </HomeLogo>
+      <div>2</div>
+      <div>3</div>
+    </Layout>
   );
 }
 
 export default App;
+
+const Layout = styled.div`
+  display: flex;
+  height: 88px;
+  border-bottom: 1px solid
+`;
+
+const HomeLogo = styled.div`
+  flex: auto;
+  max-width: 377px;
+  margin-right: 32px;
+  text-indent: -9999px;
+`;
